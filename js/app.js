cfe15be124d38d8a791db52afd3ff1fedb87b46b
lynnhao <lihao@ucsd.edu>
2016-02-11 02:26:15 -0800

    Edited padding for scrolling as well as center-justification of section titles

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 
-
 $(document).ready(function(){
     $(".section").css({"height":$(window).height()});
     $.scrollify({
         section : ".section"
     });
 
+
     $(".nav-arrow").click(function(e) {
 		e.preventDefault();
 
 		$.scrollify.next();
 	});
+
 
     /*$("#footer").css({"padding-top":$(window).height()-100});*/
 
